ab5f8c030e922f2c0f0066236fa598b123ee888b
modernfuthark <timothys1738@gmail.com>
2020-09-14 16:59:07 -0400

    [^] Updated, can just use len and ?userid to get things easier

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,29 +4,26 @@
 from requests import get
 from sys import argv
 
-url_todos = "https://jsonplaceholder.typicode.com/todos/"
+url_todos = "https://jsonplaceholder.typicode.com/todos?userId="
 url_user = "https://jsonplaceholder.typicode.com/users/"
 
 
 def GetData():
     """ Gets the data from a url """
     if len(argv) < 2:
-        print("No ID given")
-        return
+        return print("Invalid")
 
     user = get(url_user + argv[1]).json()
-    todos = get(url_todos).json()
+    todos = get(url_todos + argv[1]).json()
 
-    todos_total = 0
+    todos_total = len(todos)
     todos_done = 0
     results = []
 
     for i in todos:
-        if i["userId"] == user.get("id"):
-            todos_total += 1
-            if i["completed"]:
-                todos_done += 1
-                results.append(i["title"])
+        if i["completed"]:
+            todos_done += 1
+            results.append(i["title"])
     print("Employee {} is done with tasks({}/{})"
           .format(user.get("name"), todos_done, todos_total))
     for line in results:
