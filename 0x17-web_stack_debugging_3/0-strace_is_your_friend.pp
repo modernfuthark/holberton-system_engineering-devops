20eca2c60357925292bb27c05d43473689b3edb3
modernfuthark <timothys1738@gmail.com>
2020-09-28 20:34:05 -0400

    [N] Added files to directory

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fixes the file extension from .phpp to .php
+exec { 'Fixes misspelling on line 137':
+	command => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+	provider => shell,
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,5 @@
+**Webstack Debugging 3**\
+Tim Sninsky\
+09/30/20
+---
+Directory for the `Webstack Debugging 3` project.
